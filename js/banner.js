4f5f7463641da846ad24cc942644debc3efd5621
kelseygill <kelseygill@Galvatron.lan>
2018-10-30 11:38:44 +1100

    willisatwat

--- js/banner.js
+++ js/banner.js
@@ -1,22 +1,23 @@
 function nextimg() {
-console.log(bannerimages[currentactive])
+  console.log(bannerimages[currentactive])
   var activeelement = document.querySelector(".banner-img.active")
   removeClass("active", activeelement)
   addClass("active", bannerimages[currentactive])
-  if (currentactive == bannerimages.length-1) {
+  if (currentactive == bannerimages.length - 1) {
     currentactive = 0
   } else {
     currentactive = currentactive + 1
   }
 
-}
+
 var bannerimages = document.querySelectorAll(".banner-img")
-console.log(bannerimages)
-var currentactive = 0
+if (bannerImages) {
+  var currectactive = 0
   nextimg()
-setInterval(function() {
-  nextimg()
-}, 5000)
+  setInterval(function() {
+    nextimg()
+  }, 5000)
+}
 
 function addClass(className, element) {
   element.className = element.className + " " + className
@@ -27,16 +28,17 @@ function removeClass(className, element) {
   element.className = element.className.replace(className, " ")
 
 }
-function hasClass(className, element){
-  return element.className.indexOf(className) >=0
+
+function hasClass(className, element) {
+  return element.className.indexOf(className) >= 0
 }
 var headerToggle = document.querySelector(".header-toggle")
-headerToggle.addEventListener("click", function(){
-  var htmlelement=document.querySelector("html")
-  if (hasClass ("navopen", htmlelement)) {
+headerToggle.addEventListener("click", function() {
+  var htmlelement = document.querySelector("html")
+  if (hasClass("navopen", htmlelement)) {
     removeClass("navopen", htmlelement)
   } else {
-    addClass ("navopen", htmlelement)
+    addClass("navopen", htmlelement)
   }
 
 })
